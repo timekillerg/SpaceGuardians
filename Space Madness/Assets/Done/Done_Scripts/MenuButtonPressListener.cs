c99581445427da45c2fed6480e00f271e21f3203
elparam <elparam@gmail.com>
2014-07-31 12:47:28 +0300

    Add Promotion in Game Play

--- Space Madness/Assets/Done/Done_Scripts/MenuButtonPressListener.cs
+++ Space Madness/Assets/Done/Done_Scripts/MenuButtonPressListener.cs
@@ -1,76 +1,107 @@
-﻿using UnityEngine;
+﻿using System.ComponentModel;
+using UnityEngine;
 using System.Collections;
 using AssemblyCSharp;
 
-public class MenuButtonPressListener : MonoBehaviour {
+public class MenuButtonPressListener : MonoBehaviour
+{
 
-	public Sprite pressedSprite;
-	private Sprite notPressedSprite;
+    public Sprite pressedSprite;
+    private Sprite notPressedSprite;
 
-	void Start () {
-		notPressedSprite = ((SpriteRenderer)renderer).sprite;
-	}
-	
-	void OnMouseDown()
-	{
-		if ((GameObject.Find ("Screen Main Menu").transform.position.x>=-3 && GameObject.Find ("Screen Main Menu").transform.position.x <= 3)
-		    || (GameObject.Find ("Screen Maps").transform.position.x>=-3 && GameObject.Find ("Screen Maps").transform.position.x <= 3))
-		if (Input.GetMouseButtonDown(0)) {
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;				
-			if (Physics.Raycast (ray, out hit, 100)) {
-				if(hit.collider.gameObject == gameObject)
-				{
-					((SpriteRenderer)renderer).sprite = pressedSprite;
-				}
-			}
-		}
-	}
+    public GameObject BigBanner;
+    private bool _isBannerShown = false;
 
-	void OnMouseUp ()
-	{
-		((SpriteRenderer)renderer).sprite = notPressedSprite;
-		if ((GameObject.Find ("Screen Main Menu").transform.position.x>=-3 && GameObject.Find ("Screen Main Menu").transform.position.x <= 3)
-		    || (GameObject.Find ("Screen Maps").transform.position.x>=-3 && GameObject.Find ("Screen Maps").transform.position.x <= 3))
-		if (Input.GetMouseButtonUp(0)) {
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;				
-			if (Physics.Raycast (ray, out hit, 100)) {
-				if(hit.collider.gameObject == gameObject)
-				{						
-					MenuItemClicked();
-				}
-			}
-		}
-	}
+    void Start()
+    {
+        notPressedSprite = ((SpriteRenderer)renderer).sprite;
+    }
 
-	void MenuItemClicked() {
-		switch(tag) {
-		case "FastGameStartButton":
-			AppCore.CurrentStatus = AppCore.Status.FAST_GAME;	
-			break;
-		case "ExitGameButton":
+    void OnMouseDown()
+    {
+        if ((GameObject.Find("Screen Main Menu").transform.position.x >= -3 && GameObject.Find("Screen Main Menu").transform.position.x <= 3)
+            || (GameObject.Find("Screen Maps").transform.position.x >= -3 && GameObject.Find("Screen Maps").transform.position.x <= 3))
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    if (hit.collider.gameObject == gameObject)
+                    {
+                        ((SpriteRenderer)renderer).sprite = pressedSprite;
+                    }
+                }
+            }
+    }
+
+    void OnMouseUp()
+    {
+        ((SpriteRenderer)renderer).sprite = notPressedSprite;
+        if ((GameObject.Find("Screen Main Menu").transform.position.x >= -3 && GameObject.Find("Screen Main Menu").transform.position.x <= 3)
+            || (GameObject.Find("Screen Maps").transform.position.x >= -3 && GameObject.Find("Screen Maps").transform.position.x <= 3))
+            if (Input.GetMouseButtonUp(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    if (hit.collider.gameObject == gameObject)
+                    {
+                        MenuItemClicked();
+                    }
+                }
+            }
+    }
+
+    void MenuItemClicked()
+    {
+        switch (tag)
+        {
+            case "FastGameStartButton":
+                AppCore.CurrentStatus = AppCore.Status.FAST_GAME;
+                break;
+            case "ExitGameButton":
+                ShowBigBanner();
+                break;
+            case "HistoryButton":
+                AppCore.CurrentStatus = AppCore.Status.MAPS;
+                break;
+            case "ScoresButton":
+                AppCore.CurrentStatus = AppCore.Status.SCORES;
+                break;
+            case "DownMapButton":
+                AppCore.CurrentStatus = AppCore.Status.LEVELS_DOWN;
+                break;
+            case "SunMapButton":
+                AppCore.CurrentStatus = AppCore.Status.LEVELS_SUN;
+                break;
+            case "IceMapButton":
+                AppCore.CurrentStatus = AppCore.Status.LEVELS_ICE;
+                break;
+            case "MeteorMapButton":
+                AppCore.CurrentStatus = AppCore.Status.LEVELS_METEOR;
+                break;
+        }
+    }
+
+    void Update()
+    {
+        if (_isBannerShown && GameObject.FindGameObjectsWithTag("PromotionBig").Length == 0)
+        {
             AppCore.CurrentStatus = AppCore.Status.EXIT;
-			AppCore.Load();
-			break;
-		case "HistoryButton":
-            AppCore.CurrentStatus = AppCore.Status.MAPS;	
-			break;
-		case "ScoresButton":
-            AppCore.CurrentStatus = AppCore.Status.SCORES;
-			break;
-		case "DownMapButton":
-            AppCore.CurrentStatus = AppCore.Status.LEVELS_DOWN;
-			break;
-		case "SunMapButton":
-            AppCore.CurrentStatus = AppCore.Status.LEVELS_SUN;
-			break;
-		case "IceMapButton":
-            AppCore.CurrentStatus = AppCore.Status.LEVELS_ICE;
-			break;
-		case "MeteorMapButton":
-			AppCore.CurrentStatus =AppCore.Status.LEVELS_METEOR;
-			break;
-		}
-	}
+            AppCore.Load();
+        }
+    }
+
+    void ShowBigBanner()
+    {
+        if (BigBanner != null)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("PromotionSmall"));
+            var banner = Instantiate(BigBanner, Vector3.zero, Quaternion.identity);
+            _isBannerShown = true;
+            Destroy(banner, 3);
+        }
+    }
 }
--- Space Madness/Assets/Done/Done_Scripts/PromotionController.cs
+++ Space Madness/Assets/Done/Done_Scripts/PromotionController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using AssemblyCSharp;
 
@@ -10,24 +6,41 @@ public class PromotionController : MonoBehaviour
 {
     public GameObject Banner;
     public float SleepTime;
-    private bool isBannerClosed = false;
+
+    private bool _isBannerShown = false;
 
     void Start()
     {
-        Banner.SetActive(false);
+        Destroy(GameObject.FindGameObjectWithTag("PromotionSmall"));
     }
 
     void Update()
     {
-        if (!isBannerClosed && (AppCore.CurrentStatus == AssemblyCSharp.AppCore.Status.FAST_GAME_OVER ||
-            AppCore.CurrentStatus == AssemblyCSharp.AppCore.Status.RESTART_FAST_GAME))
-            StartCoroutine(WaitThenShow());
+        if ((AppCore.CurrentStatus == AppCore.Status.FAST_GAME_OVER ||
+             AppCore.CurrentStatus == AppCore.Status.RESTART_FAST_GAME))
+        {
+            if (_isBannerShown == false)
+            {
+                _isBannerShown = true;
+                Debug.Log("promotion created");
+                StartCoroutine(WaitThenShow());
+            }
+        }
+        else
+        {
+            if (_isBannerShown)
+            {
+                _isBannerShown = false;
+                Destroy(GameObject.FindGameObjectWithTag("PromotionBig"));
+            }
+        }
     }
 
     IEnumerator WaitThenShow()
     {
         yield return new WaitForSeconds(SleepTime);
-        Banner.SetActive(true);
+        var banner = Instantiate(Banner, Vector3.zero, Quaternion.identity);
+        Destroy(banner, 7);
     }
 }
 
